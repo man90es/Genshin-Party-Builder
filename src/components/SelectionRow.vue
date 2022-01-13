31fb0bb0b8a876682dde8de34eadece3e0a4942b
man90 <man90@tuta.io>
2022-01-13 14:34:00 +0100

    Rewrite SelectionRow using Composition API

--- src/components/SelectionRow.vue
+++ src/components/SelectionRow.vue
@@ -1,42 +1,35 @@
 <template>
 	<div class="selection-row">
-		<CharacterCard :characterID="characterID" />
+		<character-card :characterID="characterID" />
 		<div class="inputs">
-			<OwnedCheckbox :clickHandler="toggleHave" :value="characterID in $store.getters.characters" />
-			<ConstellationInput :value="($store.getters.characters[characterID] || {constellation: 0}).constellation" :incrementHandler="incrementHandler" :decrementHandler="decrementHandler" />
+			<owned-checkbox :clickHandler="toggle" :value="characterID in $store.getters.characters" />
+			<div class="constellation-input">
+				<button @click="dec">-</button>
+					C{{ constellation }}
+				<button @click="inc">+</button>
+			</div>
 		</div>
 	</div>
 </template>
 
-<script>
-	import CharacterCard from './CharacterCard.vue'
-	import OwnedCheckbox from './OwnedCheckbox.vue'
-	import ConstellationInput from './ConstellationInput.vue'
+<script setup>
+	import { defineProps, computed } from "vue"
+	import { useStore } from "vuex"
 
-	export default {
-		props: ['characterID'],
-		components: {
-			CharacterCard,
-			OwnedCheckbox,
-			ConstellationInput
-		},
-		methods: {
-			incrementHandler() {
-				this.$store.commit('updateConstellation', { id: this.characterID, value: 1 })
-			},
+	import CharacterCard from "@/components/CharacterCard.vue"
+	import OwnedCheckbox from "@/components/OwnedCheckbox.vue"
 
-			decrementHandler() {
-				this.$store.commit('updateConstellation', { id: this.characterID, value: -1 })
-			},
+	const { characterID } = defineProps(["characterID"]) // eslint-disable-line
+	const store = useStore()
 
-			toggleHave() {
-				this.$store.commit('toggleHave', { id: this.characterID })
-			}
-		}
-	}
+	const constellation = computed(() => store.getters.characters[characterID]?.constellation || 0)
+
+	const inc = () => store.commit("updateConstellation", { id: characterID, value: 1 })
+	const dec = () => store.commit("updateConstellation", { id: characterID, value: -1 })
+	const toggle = () => store.commit("toggleHave", { id: characterID })
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 	.selection-row {
 		display: grid;
 		grid-template-columns: 1fr 1fr;
@@ -47,5 +40,28 @@
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
+	}
+
+	.constellation-input {
+		display: flex;
+
+		img {
+			height: 2em;
+			width: 2em;
+			cursor: pointer;
+		}
+
+		button {
+			border: none;
+			outline: none;
+			background-color: var(--button-background-color);
+			color: var(--button-font-color);
+			margin: 0 0.5em;
+			height: 1.5em;
+			width: 1.5em;
+			font-weight: bold;
+			border-radius: 50%;
+			cursor: pointer;
+		}
 	}
 </style>
