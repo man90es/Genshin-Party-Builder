d01ad406bb60bbe0fba54d977763b64133c74c3e
man90 <man90@tuta.io>
2022-02-16 14:06:25 +0100

    Remove mitt from dependencies

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import { createApp } from "vue"
-import mitt from "mitt"
 
 import App from "./App.vue"
 import router from "./router"
 import store from "./store"
-
-window.mitt = window.mitt || new mitt()
 
 createApp(App)
 	.use(router)
