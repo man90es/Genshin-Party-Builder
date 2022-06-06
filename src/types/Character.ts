4d343670b7867ca5ff35573b8a7a39bf19883bac
man90 <man90@tuta.io>
2022-06-06 19:41:48 +0200

    Type hotfix

--- src/types/Character.ts
+++ src/types/Character.ts
@@ -1,8 +1,8 @@
 export type Character = {
-	name?: string,
 	background?: string,
-	damage: string[],
+	damage?: string[],
 	element: string,
+	name?: string,
 	roles: (string | { c: number, role: string })[],
 	score: number[],
 	stars: number,
