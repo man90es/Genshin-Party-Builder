be622d841ce9477f6392b92b52ad617cdc7ed968
man90 <man90@tuta.io>
2022-08-01 19:40:22 +0200

    Improve the suggesions algorithm

--- src/hooks/suggest.ts
+++ src/hooks/suggest.ts
@@ -1,15 +1,6 @@
+import { shuffle } from "@/utils"
 import { useStore } from "vuex"
-import type { Character, JSONData, SimpleParty } from "@/types"
-
-type ProcessedCharacter = {
-	constellation: number;
-	damage: string[];
-	element: string;
-	id: string;
-	roles: string[];
-	score: number;
-	weapon: string;
-}
+import type { Character, ProcessedCharacter, JSONData, SimpleParty } from "@/types"
 
 function processCharacter(id: string, character: Character, constellation: number): ProcessedCharacter {
 	return {
@@ -17,42 +8,58 @@ function processCharacter(id: string, character: Character, constellation: numbe
 		damage: character.damage || [character.element],
 		element: character.element,
 		id,
+		roles: character.roles.flatMap(r => (
+			"object" === typeof r
+				? constellation >= r.c ? [r.role] : []
+				: [r]
+		)),
 		score: character.score[constellation],
 		weapon: character.weapon,
-		roles: character.roles
-			.map((r) => (
-				"object" === typeof r
-					? constellation >= r.c
-						? r.role
-						: undefined
-					: r
-			))
-			.filter(Boolean) as string[],
 	}
 }
 
 function getFitness(character: ProcessedCharacter, currentParty: ProcessedCharacter[]) {
 	const scores = []
 
-	// Start with up to 6 points from the tier list
-	scores.push(character.score)
+	// Start with points from the tier list
+	scores.push(character.score / 6)
 
-	// Add 1 point for each role character has that party doesn't have
+	// Add points for each role character has that party doesn't have
 	scores.push(
 		character.roles.reduce((acc, cur) => (
 			currentParty.map(c => c.roles).flat().includes(cur)
 				? acc
 				: acc + 1
-		), 0)
+		), 0) / 7
 	)
 
-	// Subtract up to 3 points for having the same weapon type other characters in party
+	// Subtract points for having the same weapon type as other characters in party
 	scores.push(
-		-currentParty.filter(c => character.weapon === c.weapon).length
+		-currentParty.filter(c => character.weapon === c.weapon).length / 3
 	)
 
-	// TODO: Add points for potential to create resonance
-	// TODO: Add/subtract points for character-specific interactions
+	// Add a point for potential to create resonance (except for useless 4-unique resonance)
+	scores.push(
+		Number(1 === currentParty.filter(c => character.element === c.element).length)
+	)
+
+	// Add/subtract points for character-specific interactions
+	switch (character.id) {
+		case "yelan": {
+			const currentPartyElements = new Set(currentParty.map(c => c.element))
+			// When the party has 1/2/3/4 Elemental Types,
+			// Yelan's Max HP is increased by 6%/12%/18%/30%.
+			// And her skills scale off her HP
+			scores.push(
+				currentPartyElements.size + Number(!currentPartyElements.has("hydro"))
+			)
+
+			break
+		}
+
+		default:
+			scores.push(0)
+	}
 
 	return scores.reduce((acc, cur) => acc + cur, 0)
 }
@@ -65,30 +72,28 @@ export default function () {
 		const data: JSONData = store.state.data
 		const ownedCharacters = store.state.ownedCharacters
 
-		const selected = currentParty
-			.map(id => [id, id ? data.characters[id] : null])
-			.filter(pair => !pair.includes(null))
-			.map(pair => pair)
-			.map(([id, character]) => (
-				undefined !== character
-					? processCharacter(id as string, character as Character, ownedCharacters[id as string].constellation)
-					: null
-			))
-			.filter(Boolean) as ProcessedCharacter[]
-
-		const pool = Object.entries(data.characters)
+		const processedCharacters = Object.entries(data.characters)
+			// Not interested in characters that are not owned
 			.filter(([id]) => id in ownedCharacters)
-			.filter(([id]) => !currentParty.includes(id))
-			.map(([id, character]) => (
+			.flatMap(([id, character]) => (
 				undefined !== character
-					? processCharacter(id, character, ownedCharacters[id].constellation)
-					: null
+					? [processCharacter(id, character, ownedCharacters[id].constellation)]
+					: []
 			))
-			.filter(Boolean) as ProcessedCharacter[]
+
+		const selected = currentParty
+			.filter(Boolean)
+			.flatMap((id) => {
+				const found = processedCharacters.find(c => id === c.id)
+				return found ? [found] : []
+			})
+
+		const pool = processedCharacters
+			.filter(({ id }) => !(currentParty.includes(id)))
 
 		return [
 			"character",
-			pool
+			shuffle(pool)
 				.map(c => ({ characterId: c.id, fitness: getFitness(c, selected) }))
 				.sort((a, b) => a.fitness < b.fitness ? 1 : -1)
 				.slice(0, n)
--- src/types/Character.ts
+++ src/types/Character.ts
@@ -2,6 +2,7 @@ export type Character = {
 	background?: string,
 	damage?: string[],
 	element: string,
+	id: number,
 	name?: string,
 	roles: (string | { c: number, role: string })[],
 	score: number[],
