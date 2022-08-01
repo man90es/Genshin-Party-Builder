c25972187576ed9417d1d2447a7436d691142e68
man90 <man90@tuta.io>
2022-08-01 19:39:50 +0200

    Add data validation

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+export * from "./validateData"
+
 export function shuffle<Type>(array: Type[]): Type[] {
 	for (let i = array.length - 1; i > 0; i--) {
 		const j = Math.floor(Math.random() * (i + 1))
