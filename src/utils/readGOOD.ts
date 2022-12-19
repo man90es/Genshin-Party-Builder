20c3b147e62b53773f9ab762991f6260276b3217
man90 <man90@tuta.io>
2022-12-19 09:06:31 +0100

    Allow import from GOODv2 #172

--- src/utils/readGOOD.ts
+++ src/utils/readGOOD.ts
@@ -1,7 +1,7 @@
 const errors = {
-	badFormat: "Selected file is in a wrong format. Please try another file.",
-	badVersion: "The GOOD format version of the selected file is currently unsupported. Please try again later.",
-	noCharacters: "Selected file contains no character data. Please try another file.",
+	badFormat: (s1?: any) => `Selected file is in a wrong format${s1 ? " " + s1 : ""}. Please try another file.`,
+	badVersion: (s1: any) => `The GOOD format version ${s1} of the selected file is currently unsupported. Please try again later.`,
+	noCharacters: () => "Selected file contains no character data. Please try another file.",
 }
 
 export default async function readGOOD(file: File): Promise<{ characters: any[] }> {
@@ -9,22 +9,22 @@ export default async function readGOOD(file: File): Promise<{ characters: any[] 
 		const reader = new FileReader()
 
 		if ("application/json" !== file.type) {
-			return reject(errors.badFormat)
+			return reject(errors.badFormat(file.type))
 		}
 
 		reader.onload = () => {
 			const data = JSON.parse(reader.result as string)
 
 			if ("GOOD" !== data.format) {
-				return reject(errors.badFormat)
+				return reject(errors.badFormat())
 			}
 
-			if (1 !== data.version) {
-				return reject(errors.badVersion)
+			if (1 > data.version || 3 <= data.version) {
+				return reject(errors.badVersion(data.version))
 			}
 
 			if (!data.characters) {
-				return reject(errors.noCharacters)
+				return reject(errors.noCharacters())
 			}
 
 			return resolve(data)
