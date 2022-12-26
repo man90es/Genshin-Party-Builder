e4817f7fdb52759abfd4ef5917c05fd668479b83
man90 <man90@tuta.io>
2022-12-26 15:22:14 +0100

    Add hook for generating party insights

---
+++ src/hooks/insights.ts
@@ -0,0 +1,101 @@
+import _difference from "lodash/difference"
+import { computed } from "vue"
+import { useJsonDataStore } from "@/stores/jsonData"
+import { useRoute } from "vue-router"
+import { useUserDataStore } from "@/stores/userData"
+
+const reactionRequirements: { [key: string]: string[][] } = {
+	aggravate: [
+		["dendro", "electro"],
+	],
+	bloom: [
+		["dendro", "hydro"],
+	],
+	burgeon: [
+		["hydro", "dendro", "pyro"],
+	],
+	burning: [
+		["pyro", "dendro"],
+	],
+	crystallise: [
+		["geo", "cryo"],
+		["geo", "electro"],
+		["geo", "hydro"],
+		["geo", "pyro"],
+	],
+	electrocharged: [
+		["electro", "hydro"],
+	],
+	frozen: [
+		["hydro", "cryo"],
+	],
+	hyperbloom: [
+		["hydro", "dendro", "electro"],
+	],
+	melt: [
+		["pyro", "cryo"],
+	],
+	overloaded: [
+		["pyro", "electro"],
+	],
+	quicken: [
+		["dendro", "electro"],
+	],
+	spread: [
+		["dendro", "electro"],
+	],
+	superconduct: [
+		["cryo", "electro"],
+	],
+	swirl: [
+		["anemo", "cryo"],
+		["anemo", "electro"],
+		["anemo", "hydro"],
+		["anemo", "pyro"],
+	],
+	vaporise: [
+		["hydro", "pyro"],
+	],
+}
+
+export function useInsights() {
+	const jsonData = useJsonDataStore()
+	const route = useRoute()
+	const userData = useUserDataStore()
+
+	const party = computed(() => {
+		const idx = Number(route.params.index)
+		return userData.parties[idx].members.filter(Boolean) as string[]
+	})
+
+	const reactions = computed(() => {
+		if (!party.value) {
+			return []
+		}
+
+		const partyElements = party.value.map(c => jsonData.characters[c!]?.element)
+
+		return Object.entries(reactionRequirements)
+			.flatMap(([name, options]) => (
+				options.map(o => _difference(o, partyElements).length === 0).includes(true)
+					? [name]
+					: []
+			))
+	})
+
+	const resonances = computed(() => {
+		if (!party.value) {
+			return []
+		}
+
+		const partyElements = party.value.map(c => jsonData.characters[c!]?.element)
+
+		return Object.entries(partyElements.reduce((prev, cur) => ({
+			...prev,
+			[cur]: prev[cur] ? prev[cur] + 1 : 1
+		}), {} as { [key: string]: number }))
+			.flatMap(([element, n]) => n > 1 ? [element] : [])
+	})
+
+	return { reactions, resonances }
+}
