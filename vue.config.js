2709801012a7fe5c3b327fe16ab9ae1e1f4d8f45
man90 <man90@tuta.io>
2023-11-02 18:06:26 +0100

    Bump dependencies' versions

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === "production"
-		? process.env.VUE_APP_PUBLIC_PATH || "/genshin-party-builder/"
-		: "/"
+	publicPath: process.env.VUE_APP_PUBLIC_PATH || "/"
 }
